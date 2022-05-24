89d309f40fc411b481d302785ac543b52ec118a8
Desmond <desezo@gmail.com>
2022-05-24 17:24:13 +0100

    making order return

--- app/Models/order_items.php
+++ app/Models/order_items.php
@@ -7,5 +7,5 @@ use Illuminate\Database\Eloquent\Model;
 class order_items extends Model
 {
     protected $table = "order_items";
-    protected $fillable = ["order_id"];
+    protected $fillable = ["order_id", "product_id"];
 }
