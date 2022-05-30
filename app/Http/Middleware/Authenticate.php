214a98c0c505201419ee149e6a5e8eef30756d22
Desmond <desezo@gmail.com>
2022-05-30 08:48:04 +0100

    response update

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -36,8 +36,8 @@ class Authenticate
     public function handle($request, Closure $next, $guard = null)
     {
         if ($this->auth->guard($guard)->guest()) {
-//            return response('Unauthorized.', 401);
-            return  ['status' => '401', 'desc' =>'You are unauthorized'];
+            return response(["code" => 'Unauthorized'], 401);
+//            return  ['status' => '401', 'desc' =>'You are unauthorized'];
 
         }
 
