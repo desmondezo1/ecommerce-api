52f7d5e782d6f843f8772bb1006505b60eda5404
Desmond <desezo@gmail.com>
2022-06-24 19:27:25 +0100

    address fix

--- app/Models/billing_address.php
+++ app/Models/billing_address.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class billing_address extends Model
 {
     protected $table = "billing_addresses";
-    protected $fillable = ['is_company'];
+    protected $guarded = [];
 
     public function user(){
         $this->hasOne(User::class);
