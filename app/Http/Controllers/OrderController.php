df881f4330f4b2ae53f3b392e7a93046d62011e9
Desmond <desezo@gmail.com>
2022-08-07 03:36:15 +0100

    order details

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -39,7 +39,7 @@ class OrderController extends Controller
         $prd_details=[];
 
         $prods = json_decode($order->product_id, true);
-        return $prods[0]['id'];
+
          for ($i = 0; count($prods) > $i; $i++){
             $prd_details[] = product::find($prods[$i]['id']);
          }
