c0d3aaa8a0808e1801ca8d459f1fb31d7a52220e
Desmond <desezo@gmail.com>
2022-09-12 16:52:13 +0100

    fixing wishlist issue

--- app/Models/Wishlist.php
+++ app/Models/Wishlist.php
@@ -7,8 +7,5 @@ use Illuminate\Database\Eloquent\Model;
 class Wishlist extends Model
 {
     protected $table = 'wishlist';
-    protected $fillable = [
-        'user_id',
-        'product_id',
-    ];
+    protected $guarded = [];
 }
