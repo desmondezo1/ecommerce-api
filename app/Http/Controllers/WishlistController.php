79bc9496da7fae72325053af3ec114ce1c7dec84
Desmond <desezo@gmail.com>
2022-09-13 06:02:33 +0100

    fixing wishlist issue

--- app/Http/Controllers/WishlistController.php
+++ app/Http/Controllers/WishlistController.php
@@ -32,7 +32,7 @@ class WishlistController extends Controller
         $list = [];
         foreach ($wishlist as $wish){
             $list[] = product::find($wish['product_id']);
-            $list['picture'] = productImages::where('product_id', $wish['product_id'])->first();
+            $list['photo'] = productImages::where('product_id', $wish['product_id'])->first();
         }
 
         return ['status' => 200, 'desc' => 'Wishlist fetched successfully', 'data'=> $list ];
