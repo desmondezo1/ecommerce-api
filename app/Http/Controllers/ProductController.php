6327cd8b9dc17590021ecbc85d564a4ab7c6a197
Desmond <desezo@gmail.com>
2022-09-14 23:34:13 +0100

    edit brand

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -26,8 +26,9 @@ class ProductController extends Controller
             $categories = product::find($product->id)->images;
             $images = $images->toArray();
             $product['photo'] = "https://via.placeholder.com/150";
-//            $brand = partner::find($product->brand_id);
-//            $product['brand'] = $brand['name'];
+            $brand = partner::find($product->brand_id);
+            dd($brand);
+            $product['brand'] = $brand['name'];
             if(!empty($images)){
               $product['photo'] = $images[0]['image'];
             }
